b3622487baae052e81bacb4fd3b83c478f3b0500
ftrbnd <giosalas25@gmail.com>
2023-04-14 13:50:10 -0700

    Add Gameboard class

---
+++ src/classes/Gameboard.js
@@ -0,0 +1,75 @@
+const Gameboard = (rows = 8, cols = 8) => {
+    rows = rows <= 8 ? 8 : rows; // enforce minimum board size
+    cols = cols <= 8 ? 8 : cols;
+
+    let board = [];
+    for (let r = 0; r < rows; r++) {
+        board.push([]);
+        for (let c = 0; c < cols; c++) {
+            board[r].push('_');
+        }
+    }
+
+    const shipCoords = [];
+
+    const placeShip = (ship, x, y, isVertical) => {
+        const prevBoard = JSON.parse(JSON.stringify(board));
+        const coords = [];
+
+        if (isVertical) {
+            for (let i = 0; i < ship.getLength(); i++) {
+                if (x < 0 || x >= board.length) {
+                    board = prevBoard;
+                    throw new RangeError(`Cannot vertically place ship on row ${x}!`);
+                }
+
+                coords.push([x, y]);
+                board[x++][y] = 'S';
+            }
+        } else {
+            for (let i = 0; i < ship.getLength(); i++) {
+                if (y < 0 || y >= board[0].length) {
+                    board = prevBoard;
+                    throw new RangeError(`Cannot horizontally place ship on column ${y}!`);
+                }
+                
+                coords.push([x, y]);
+                board[x][y++] = 'S';
+            }
+        }
+
+        shipCoords.push([ship, coords]);
+    };
+
+    const receiveAttack = (x, y) => {
+        if (board[x][y] == 'S') { // hit
+            board[x][y] = 'H';
+
+            // register hit on respective ship
+            for (const [ship, coords] of shipCoords) {
+                if (x == coords[0] && y == coords[1]) {
+                    ship.hit(1);
+                }
+            }
+
+        } else { // miss
+            board[x][y] = 'M';
+        }
+    };
+
+    const allShipsSunk = () => {
+        for (const ship of ships) {
+            if (!ship.isSunk()) return false;
+        }
+        return true;
+    }
+
+    return {
+        board,
+        placeShip,
+        receiveAttack,
+        allShipsSunk
+    }
+}
+
+export default Gameboard;
