a29921ee1e3e743bae1535b04ab211849e7241f9
ftrbnd <giosalas25@gmail.com>
2023-04-18 15:56:19 -0700

    Set up both players' boards

--- src/index.js
+++ src/index.js
@@ -9,23 +9,30 @@ function game() {
     const SIDE_LENGTH = 10;
     const boardOne = Gameboard(SIDE_LENGTH, SIDE_LENGTH);
     const boardTwo = Gameboard(SIDE_LENGTH, SIDE_LENGTH);
+    populateBoards(SIDE_LENGTH, boardOne, boardTwo);
 
-    const shipLengths = [5, 4, 3, 2, 2];
-    while (shipLengths.length > 0) {
-        const length = shipLengths.pop();
-        const newShip = Ship(length);
+    console.log('Board 1: ', boardOne.getBoard());
+    console.log('Board 2: ', boardTwo.getBoard());
+}
 
-        const x = Math.floor(Math.random() * SIDE_LENGTH);
-        const y = Math.floor(Math.random() * SIDE_LENGTH);
-        const isVertical = Math.random() < 0.5;
+function populateBoards(sideLength, ...boards) {
+    for (const board of boards) {
+        const shipLengths = [5, 4, 3, 2, 2];
+        
+        while (shipLengths.length > 0) {
+            const length = shipLengths.pop();
+            const newShip = Ship(length);
 
-        const placedShip = boardOne.placeShip(newShip, x, y, isVertical, length);
-        if (!placedShip) {
-            shipLengths.push(length);
+            const x = Math.floor(Math.random() * sideLength);
+            const y = Math.floor(Math.random() * sideLength);
+            const isVertical = Math.random() < 0.5;
+
+            const placedShip = board.placeShip(newShip, x, y, isVertical, length);
+            if (!placedShip) {
+                shipLengths.push(length);
+            }
         }
     }
-
-    console.log('Final board: ', boardOne.getBoard());
 }
 
 game();
