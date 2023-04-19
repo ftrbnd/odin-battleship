1165fb6f0c353ac20258afa3db65e8dc27bbd3d8
ftrbnd <giosalas25@gmail.com>
2023-04-19 12:04:04 -0700

    Separate functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Gameboard from "./classes/Gameboard";
 import Player from "./classes/Player";
-import Ship from "./classes/Ship";
+import { displayBoard, populateBoards } from "./ui/Gameboard.display";
 
 function game() {
     const playerOne = Player('P1');
@@ -16,46 +16,6 @@ function game() {
 
     console.log('Board 1: ', boardOne.getBoard());
     console.log('Board 2: ', boardTwo.getBoard());
-}
-
-function populateBoards(sideLength, ...boards) {
-    for (const board of boards) {
-        const shipLengths = [5, 4, 3, 3, 2];
-        
-        while (shipLengths.length > 0) {
-            const length = shipLengths.pop();
-            const newShip = Ship(length);
-
-            const x = Math.floor(Math.random() * sideLength);
-            const y = Math.floor(Math.random() * sideLength);
-            const isVertical = Math.random() < 0.5;
-
-            const placedShip = board.placeShip(newShip, x, y, isVertical, length);
-            if (!placedShip) {
-                shipLengths.push(length);
-            }
-        }
-    }
-}
-
-function displayBoard(board, id) {
-    const boardDiv = document.getElementById(id);
-    boardDiv.style.display = 'grid';
-    boardDiv.style.gridTemplateRows = `repeat(${board.getRows()}, 64px)`;
-    boardDiv.style.gridTemplateColumns = `repeat(${board.getRows()}, 64px)`;
-
-    for (let r = 0; r < board.getRows(); r++) {
-        for (let c = 0; c < board.getColumns(); c++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.setAttribute('id', `${id}-cell-${r}-${c}`);
-
-            cell.textContent = board.getBoard()[r][c];
-
-            boardDiv.appendChild(cell)
-        }
-
-    }
 }
 
 game();
---
+++ src/ui/Gameboard.display.js
@@ -0,0 +1,51 @@
+import Ship from "../classes/Ship";
+
+export function displayBoard(board, id) {
+    const boardDiv = document.getElementById(id);
+    boardDiv.style.gridTemplateRows = `repeat(${board.getRows()}, 64px)`;
+    boardDiv.style.gridTemplateColumns = `repeat(${board.getRows()}, 64px)`;
+
+    for (let r = 0; r < board.getRows(); r++) {
+        for (let c = 0; c < board.getColumns(); c++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.setAttribute('id', `${id}-cell-${r}-${c}`);
+
+            switch (board.getBoard()[r][c]) {
+                case 5:
+                case 4:
+                case 3:
+                case 2:
+                    cell.style.backgroundColor = 'lightgray';
+                    break;
+                case '_':
+                default:
+                    break;
+            }
+
+            boardDiv.appendChild(cell);
+        }
+
+    }
+}
+
+// temp function - let users place ships later
+export function populateBoards(sideLength, ...boards) {
+    for (const board of boards) {
+        const shipLengths = [5, 4, 3, 3, 2];
+        
+        while (shipLengths.length > 0) {
+            const length = shipLengths.pop();
+            const newShip = Ship(length);
+
+            const x = Math.floor(Math.random() * sideLength);
+            const y = Math.floor(Math.random() * sideLength);
+            const isVertical = Math.random() < 0.5;
+
+            const placedShip = board.placeShip(newShip, x, y, isVertical, length);
+            if (!placedShip) {
+                shipLengths.push(length);
+            }
+        }
+    }
+}
