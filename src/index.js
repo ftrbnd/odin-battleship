9a383ddad7c4e53b723b7d30659e3daef402b299
ftrbnd <giosalas25@gmail.com>
2023-04-20 13:44:57 -0700

    Separate done functions

--- src/index.js
+++ src/index.js
@@ -28,36 +28,46 @@ function setup(board_length = 10) {
     });
 
     player1DoneButton.addEventListener('click', () => {
-        document.querySelector('#board1').style.display = 'none';
-        document.querySelector('#enemy1').style.display = 'none';
-        document.querySelector('#board2').style.display = 'grid';
-        document.querySelector('#enemy2').style.display = 'grid';
-
-        displayBoard(boardTwo, 'board2', true);
-        displayEnemyBoard(boardOne, 'enemy2', playerOne, true);
-
-        player1DoneButton.style.pointerEvents = 'none';
-        player2DoneButton.style.pointerEvents = 'auto';
+        playerOneDone(boardOne, boardTwo, playerOne, player1DoneButton, player2DoneButton);
     });
     
     player2DoneButton.addEventListener('click', () => {
-        document.querySelector('#board2').style.display = 'none';
-        document.querySelector('#enemy2').style.display = 'none';
-
-        document.querySelector('#board1').style.display = 'grid';
-        document.querySelector('#enemy1').style.display = 'grid';
-
-        displayBoard(boardOne, 'board1', true);
-        displayEnemyBoard(boardTwo, 'enemy1', playerTwo, true);
-
-        player2DoneButton.style.pointerEvents = 'none';
-        player1DoneButton.style.pointerEvents = 'auto';
+        playerTwoDone(boardTwo, boardOne, playerTwo, player2DoneButton, player1DoneButton);
     });
+}
+
+function playerOneDone(board, enemyBoard, player, button, enemyButton) {
+    document.querySelector('#board1').style.display = 'none';
+    document.querySelector('#enemy1').style.display = 'none';
+    document.querySelector('#board2').style.display = 'grid';
+    document.querySelector('#enemy2').style.display = 'grid';
+
+    displayBoard(enemyBoard, 'board2', true);
+    displayEnemyBoard(board, 'enemy2', player, true);
+
+    button.style.pointerEvents = 'none';
+    enemyButton.style.pointerEvents = 'auto';
+}
+
+function playerTwoDone(board, enemyBoard, player, button, enemyButton) {
+    document.querySelector('#board2').style.display = 'none';
+    document.querySelector('#enemy2').style.display = 'none';
+    document.querySelector('#board1').style.display = 'grid';
+    document.querySelector('#enemy1').style.display = 'grid';
+
+    displayBoard(enemyBoard, 'board1', true);
+    displayEnemyBoard(board, 'enemy1', player, true);
+
+    button.style.pointerEvents = 'none';
+    enemyButton.style.pointerEvents = 'auto';
 }
 
 setup();
 
 /**
  * TODO:
- * only let players click on one cell
+ * clean up functions
+ * add game over announcement
+ * announce who's turn it is
+ * add keyboard shortcut as alternative to turn-button
  */
